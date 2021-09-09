4901be976f16cf4b0379ae8f1beceb7af526c1a9
Jabez Sanjay <jbzsanjay2000@gmail.com>
2021-09-10 01:05:16 +0530

    Completed Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Navbar from "./Components/Navbar";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="min-h-screen bg-bodyColor">
+      <Navbar />
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      bodyColor: "#F0F0F7",
+      white: "#ffffff",
+      mainColor: "#43425D",
+      lightGray: "#4D4F5C",
+    },
   },
   variants: {
     extend: {},
